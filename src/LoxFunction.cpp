5c55a219912ddd745db2e6b20bdd13cddcd0c309
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-10-13 20:38:02 -0400

    Fixed return exception

--- src/LoxFunction.cpp
+++ src/LoxFunction.cpp
@@ -11,10 +11,12 @@ std::any LoxFunction::Call(Interpreter* interpreter, const std::vector<std::any>
 	for (int i = 0; i < decleration->params.size(); i++) {
 		environment->Define(decleration->params[i].lexeme, arguments[i]);
 	}
+	Environment* previous = interpreter->environment;
 	try {
 		interpreter->ExecuteBlock(decleration->body, environment);
 	}
 	catch (const ReturnException& e) {
+		interpreter->environment = previous;
 		return e.value;
 	}
 	return std::any();
